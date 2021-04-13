92a55bbabe69ac5c967b5a356cef7ddd057c213f
brunoventuras <brunoventuras@gmail.com>
2021-04-13 13:46:03 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -5,13 +5,35 @@ const routes = express.Router();
 const caminho = 'archive/whats-082.txt';
 
 routes.get('/listar',(req, res)=>{
-
+let result = new Array
+let data, horario, contato, status, altStatus, grupo
   const fileBuffer = fs.readFileSync(caminho,'utf-8')
-  let data = fileBuffer.split(/\n/g)
-  console.log(data)
+  let lines = fileBuffer.split(/\n/g)
+  let arrLines = lines.map((e,i)=>{
+    e.substr(24,5)=='criou'?grupo = e.substr(37):false
+
+    data = e.substr(0, 10)
+    horario = e.substr(11, 5)
+    if(e.substr(20,1)=='+'){
+      contato=e.substr(21,16).trim()
+      altStatus=e.substr(37).trim()
+      altStatus.substr(0,6) == 'entrou'?status="entrou":status=altStatus
+      result.push({
+        "grupo":grupo.substr(3,3),
+        "data":data,
+        "horario":horario,
+        "contato":contato,
+        "status":status
+      })
+    }else{
+      contato=null
+    }
+
+  })
+  // console.log(grupo)
   res
     .set({ 'Content-Type': 'text/plain' })
-    .send(data);
+    .send(result);
 
 
 })
